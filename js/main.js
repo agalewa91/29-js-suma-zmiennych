0672ffe97262650f2d09945ce26763e1b18a6d31
agatalewa <agatalewandowska91@gmail.com>
2023-06-19 13:04:56 +0200

    suma zmiennych

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 10;
+let secondNumber = 100;
+
+function sum(firstNumber, secondNumber) {
+  let result = firstNumber + secondNumber;
+
+  return result;
+}
+
+console.log(sum(10, 100));
